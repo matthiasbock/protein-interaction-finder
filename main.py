4e9fd53ae70ab8e595493829800a9bc3f6030a91
matthias.bock <matthias.bock@hu-berlin.de>
2012-01-09 17:26:50 +0000

    show something

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
+filename = 'BIOGRID-GENE-34444-3.1.84.tab2.txt';
 
+protein1 = 7
+protein2 = 8
+interaction = 11
+
+for line in open(filename).readlines():
+	if line[0] != "#":
+		fields = line.split('\t')
+		if fields[interaction].find('Genetic') < 0:
+			print fields[interaction]+"\t\t"+fields[protein1]+"\t"+fields[protein2]
